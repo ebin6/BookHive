5df2f3c7165345b96114a6d18498b9e031a46663
ebin6 <ebin.oneteam@gmail.com>
2025-11-27 19:59:28 +0530

    User register completed

--- credentials/views.py
+++ credentials/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -8,6 +10,12 @@ def register(request):
         last_name=request.POST["last_name"]
         mail=request.POST['email']
         password=request.POST['password']
+        if User.objects.filter(username=mail).exists():
+            return HttpResponse("User already exists")
+        else:
+            user=User.objects.create_user(username=mail,first_name=first_name,last_name=last_name,email=mail,password=password)
+            user.save()
+            return redirect("signin")
     return render(request,"register.html")
 
 def login(request):
