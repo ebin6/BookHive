5e0a78e5eb8c50f76d7bcc8fd3cc886a32f9eca4
ebin6 <ebin.oneteam@gmail.com>
2025-12-01 19:36:40 +0530

    Created Category model and admin class

--- manager/models.py
+++ manager/models.py
@@ -12,9 +12,21 @@ class Author(models.Model):
     def __str__(self):
         return self.name
     
-    def save(self):
+    def save(self,*args,**kwargs):
         if not self.slug:
             self.slug=slugify(self.name)
-        return super().save()
+        return super().save(*args,**kwargs)
     
 
+class Category(models.Model):
+    category=models.CharField(max_length=30,unique=True)
+    slug=models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.category
+    
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug=slugify(self.category)
+        return super().save(*args,**kwargs)
+    
