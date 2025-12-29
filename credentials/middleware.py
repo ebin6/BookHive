74cf99e72470d5f5749bdd37d00459910ee3891a
ebin6 <ebin.oneteam@gmail.com>
2025-12-29 20:29:28 +0530

    Role based access for CRUD

--- credentials/middleware.py
+++ credentials/middleware.py
@@ -5,7 +5,9 @@ class LoginRequiredMiddleware:
         self.get_response=get_response
     
     def __call__(self, request):
-
+        if request.path.startswith("/user/") or request.path.startswith("/manager/"):
+             if not request.user.is_authenticated:
+                  return redirect("signin")
         if request.path.startswith("/manager/") and request.user.profile.role=="user":
                 return HttpResponse("Access denied")
                 
